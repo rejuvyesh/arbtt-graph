ecfcc3d500c9a552752f3a05bcc603388b45a158
rejuvyesh <mail@rejuvyesh.com>
2014-12-01 22:48:12 +0530

    correct tags

--- render/settings.js
+++ render/settings.js
@@ -1,8 +1,8 @@
 // Barcode view settings
 var display_groups = [];
 // Productive
-display_groups.push(["read:djvu", "read:pdf", "read:epub", "read:mobi", "code:web", "code:python", "code:haskell", "code:ruby", "code:matlab", "write", "term", "mail"]);
+display_groups.push(["read-djvu", "read-pdf", "read-epub", "read-mobi", "code-web", "code-python", "code-haskell", "code-ruby", "code-matlab", "write", "term", "mail"]);
 // Unproductive
-display_groups.push(["irc", "social", "communities", "tv:local", "tv:web", "read:rss"]);
+display_groups.push(["irc", "social", "communities", "tv-local", "tv-web", "read-rss"]);
 // Don't know
 display_groups.push(["misc"])
