e44554d31b3b2565e1838d4f3d26fae0617567f1
rejuvyesh <mail@rejuvyesh.com>
2014-12-01 22:10:18 +0530

    render misc as separate

--- render/settings.js
+++ render/settings.js
@@ -4,3 +4,5 @@ var display_groups = [];
 display_groups.push(["read:djvu", "read:pdf", "read:epub", "read:mobi", "code:web", "code:python", "code:haskell", "code:ruby", "code:matlab", "write", "term", "mail"]);
 // Unproductive
 display_groups.push(["irc", "social", "communities", "tv:local", "tv:web", "read:rss"]);
+// Don't know
+display_groups.push(["misc"])
